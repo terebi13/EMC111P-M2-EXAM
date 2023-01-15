0fcb3bb167f01f826cdaa9181c9f7c5074efdd1d
Debbie Getigan <122215340+terebi13@users.noreply.github.com>
2023-01-15 23:49:14 +0800

    Add files via upload

---
+++ m2/Getigan_Bedroom.js
@@ -0,0 +1,11 @@
+import * as THREE from "three";
+
+const scene = new THREE.Scene();
+scene.background = new THREE.Color(0x000000);
+
+// Add a cube to the scene
+const geometry = new THREE.BoxGeometry(3, 1, 3); // width, height, depth
+const material = new THREE.MeshLambertMaterial({ color: 0xfb8e00 });
+const mesh = new THREE.Mesh(geometry, material);
+mesh.position.set(0, 0, 0); // Optional, 0,0,0 is the default
+scene.add(mesh);
